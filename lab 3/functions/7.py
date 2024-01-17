86ad3a4cdbea5ffe4512eed4e1bcb5152abc5e8f
MihailAexx <m_bulushev@kbtu.kz>
2024-01-17 17:51:33 +0600

    a

---
+++ lab 3/functions/7.py
@@ -0,0 +1,11 @@
+def has_33(nums):
+    for i in range(len(nums)-1):
+        if nums[i] == 3 and nums[i+1] == 3:
+            print("True")
+            return
+    print("False")
+    return
+
+has_33([1, 3, 3])
+has_33([1, 3, 1, 3])
+has_33([3, 1, 3])
