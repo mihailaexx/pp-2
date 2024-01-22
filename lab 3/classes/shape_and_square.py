e2e45d2de87542be98a1f05199d8dc65f9a3f09b
MihailAexx <m_bulushev@kbtu.kz>
2024-01-22 12:08:13 +0600

    update

--- lab 3/classes/shape_and_square.py
+++ lab 3/classes/shape_and_square.py
@@ -3,11 +3,12 @@ class Shape:
         self.length = 0
         self.width = 0
     def area(self):
-        return "0"
+        return 0
 class Square(Shape):
     def __init__(self, length):
         if not isinstance(length, int):
             raise TypeError("length must be int")
+        super().__init__()
         self.length = length
     def area(self):
         return self.length * self.length
